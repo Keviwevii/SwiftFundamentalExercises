ccbaa2e81bba12469095d90fb52ca994eee93ab0
Kevin Floyd <kfloydy@gmail.com>
2023-03-26 22:55:26 -0500

    adding bank account simulator

---
+++ BankAccountSimulator.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Ultimately, you will code a bank account simulator by creating a valid bank account and then use appropriate programming logic to manage it. You will configure the bank account simulator to check the bank account balance, validate withdrawals from the bank account and monitor deposits into the bank account.
+
+class BankAccountInterface {
+    
+    func GreetCustomer() {
+        print("Welcome to your virtual bank system.")
+    }
+    func AccountSelection() {
+        print("What account would you like to open?")
+        print("1. Debit account")
+        print("2. Credit account")
+    }
+    
+    var accountType = ""
+    
+    func CreateBankAccountType (numberPadKey: Int) {
+        print("The selected option is \(numberPadKey).")
+    }
+}
+
+
