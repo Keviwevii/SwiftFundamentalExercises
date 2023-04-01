5fc96659ae2915c42462a64a373283e221dd804e
Kevin Floyd <kfloydy@gmail.com>
2023-03-31 21:22:24 -0500

    adding exercise for Reverse String exercise

---
+++ Reversestring.playground/Contents.swift
@@ -0,0 +1,7 @@
+// Come up with a function that takes an input string and reverses its order. For example, “I love Codecademy!” should return “!ymedacedoC evol I.”
+
+func reverseString(word: String) -> String {
+    return String(word.reversed())
+}
+
+print(reverseString(word: "Tiger"))
