1c956f9ef419dfbd491ce86c0f4bcf1743aa9973
Kevin Floyd <kfloydy@gmail.com>
2023-04-12 18:14:37 -0500

    adding Reservations project and isogram

--- Isogram.playground/Contents.swift
+++ Isogram.playground/Contents.swift
@@ -1,1 +1,13 @@
+// An isogram is a word that has no repeated letters. For example, “today” is an isogram, but “tomorrow” is not. Create a function that accepts an input string and returns TRUE if the word is an isogram and FALSE if the word is not.
 
+func isIsogram(_ word: String){
+    for char in word {
+        for char in word{
+            if char == char {
+            
+            }
+        }
+    }
+    
+}
+isIsogram("tiger")
---
+++ Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Kevin Floyd on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
